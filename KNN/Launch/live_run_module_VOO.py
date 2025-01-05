20d593a7f9683dcc55752cc9de75c2fb3009eb86
Abraham001l <abraham51lbucio@gmail.com>
2025-01-04 18:38:41 -0600

    possibly working code for daily runs

--- KNN/Launch/live_run_module_VOO.py
+++ KNN/Launch/live_run_module_VOO.py
@@ -1,11 +1,10 @@
-import schedule
-import time
 import pytz
 from datetime import datetime
 import os
 from KNN.Launch.data_collection_module import update_dataset
 import pickle
 import pandas as pd
+from apscheduler.schedulers.background import BlockingScheduler
 
 # ---------- Setting Up Directory ----------
 cur_dir = os.getcwd()
@@ -17,7 +16,12 @@ model_filename = 'VOO_2020-10-15_2023-12-29.pkl'
 data_filename = os.path.join(cur_dir, 'KNN\\Development\\Datasets', data_filename)
 model_filename = os.path.join(cur_dir, 'KNN\\Development\\Models', model_filename)
 
-# ---------- Setting Up Functions ----------
+# ---------- Important Global Variables ----------
+invested = False
+scheduler = BlockingScheduler()
+central_tz = pytz.timezone('US/Central')
+
+# ---------- Setting Up Functions For Daily Runs ----------
 def run_model():
     # Updating VOO_all.csv & Reading In Data
     update_dataset(ticker)
@@ -26,29 +30,10 @@ def run_model():
     # Running Model
     model = pickle.load(open(model_filename, 'rb'))
     inputs = pd.DataFrame([data.iloc[-1][['MACD (%)', '% Distance 200MA', 'Volume Ratio', 'ATR', 'RSI', 'Volatility']]])
-    prediction = model.predict(inputs)[0]
+    invested = model.predict(inputs)[0] == 1
+
+# ---------- Setting Up Functions For Hourly Runs ----------
+def run_investment_algorithm():
     None
-
-central_tz = pytz.timezone('US/Central')
-schedule.every().sunday.at("20:00", central_tz).do(run_model)
-schedule.every().monday.at("20:00", central_tz).do(run_model)
-schedule.every().tuesday.at("20:00", central_tz).do(run_model)
-schedule.every().wednesday.at("20:00", central_tz).do(run_model)
-schedule.every().thursday.at("20:00", central_tz).do(run_model)
-
 
 # ---------- Live Run Loop ----------
-invested = False
-while True:
-    if invested:
-        None
-    else:
-
-    None
-
-
-central_tz = pytz.timezone('US/Central')
-schedule.every().day.at("10:45", central_tz).do(job)
-
-while True:
-    if invested:
--- KNN/Launch/tester_file.py
+++ KNN/Launch/tester_file.py
@@ -1,12 +1,43 @@
-from data_collection_module import update_dataset
-import os
-import pandas as pd
+# from data_collection_module import update_dataset
+# import os
+# import pandas as pd
 
-cur_dir = os.getcwd()
+# cur_dir = os.getcwd()
 
-data_filename = 'VOO_all.csv'
-data_filename = os.path.join(cur_dir, 'KNN\\Development\\Datasets', data_filename)
+# data_filename = 'VOO_all.csv'
+# data_filename = os.path.join(cur_dir, 'KNN\\Development\\Datasets', data_filename)
 
-update_dataset('VOO')
-data = pd.read_csv(data_filename)
-print(data.iloc[-1])
+# update_dataset('VOO')
+# data = pd.read_csv(data_filename)
+# print(data.iloc[-1])
+
+# import yfinance as yf
+# import pytz
+
+# central_tz = pytz.timezone('US/Central')
+# data = yf.download('VOO', start='2025-01-03', interval='1h')
+# data.index = data.index.tz_convert(central_tz)
+# print(data)
+
+from apscheduler.schedulers.background import BlockingScheduler
+
+# def job():
+#     print('ran')
+#     scheduled_id.remove()
+#     scheduler.shutdown(wait=False)
+
+# scheduler = BlockingScheduler()
+# scheduled_id = scheduler.add_job(job, 'interval', seconds = 3)
+
+# while True:
+#     scheduler.start()
+#     print('im here')
+#     scheduler = BlockingScheduler()
+#     scheduled_id = scheduler.add_job(job, 'interval', seconds = 3)
+
+
+
+# from datetime import datetime
+
+# x = datetime.now()
+# print(x.isoweekday())
